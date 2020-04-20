c814071aa0f701a89d677829971d034080e14361
meeting20 <meeting20@JVC-JEIN.CO.ID>
2020-04-20 16:54:18 +0700

    adit menambahkan filter

--- MyApp/classic/src/view/main/List.js
+++ MyApp/classic/src/view/main/List.js
@@ -15,14 +15,23 @@ Ext.define('MyApp.view.main.List', {
         type: 'personnel'
     },
 
-    plugins: {
-        ptype : 'rowediting'
-    },
+    plugins: [
+        {ptype : 'rowediting'},
+        {ptype : 'gridfilters'}
+    ],
 
     columns: [
         // { text: 'id',  dataIndex: 'id',width:100},
-        { text: 'nama',  dataIndex: 'nama', editor: 'textfield',width:250 },
-        { text: 'birthplace', dataIndex: 'birthplace', editor: 'textfield',width:250},
+        { text: 'nama',  dataIndex: 'nama', editor: 'textfield',width:250,
+            filter:{
+                type:'string'
+            }
+        },
+        { text: 'birthplace', dataIndex: 'birthplace', editor: 'textfield',width:250,
+            filter:{
+                type:'string'
+            } 
+        },
         { text: 'birthday', dataIndex: 'birthday',editor:'textfield',width:250},
         { text: 'created_at', dataIndex: 'created_at',width:250},
         { text: 'updated_at', dataIndex: 'updated_at',width:250},
